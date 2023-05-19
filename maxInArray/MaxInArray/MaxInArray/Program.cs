d0fafe527ff086b3ea9d284919d169eabcc454e6
NIRAJ <niraj@rajniraj.com>
2023-05-19 19:11:57 +0530

    [Niraj] Added : Added find maximum in float

--- maxInArray/MaxInArray/MaxInArray/Program.cs
+++ maxInArray/MaxInArray/MaxInArray/Program.cs
@@ -6,17 +6,40 @@
     {
         static void Main()
         {
-            float[] numbers = { 5.2f, 10.7f, 3.1f, 8.9f, 1.4f };
+            int[] intNumbers = { 5, 10, 3, 8, 1 };
+            float[] floatNumbers = { 5.2f, 10.7f, 3.1f, 8.9f, 1.4f };
 
-            float maximum = FindMaximum(numbers);
-            Console.WriteLine("The maximum number is: " + maximum);
+            int intMaximum = FindMaximum(intNumbers);
+            float floatMaximum = FindMaximum(floatNumbers);
+
+            Console.WriteLine("The maximum integer is: " + intMaximum);
+            Console.WriteLine("The maximum float is: " + floatMaximum);
+        }
+
+        static int FindMaximum(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                throw new ArgumentException("The integer array is empty or null.");
+            }
+
+            int maximum = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > maximum)
+                {
+                    maximum = arr[i];
+                }
+            }
+
+            return maximum;
         }
 
         static float FindMaximum(float[] arr)
         {
             if (arr == null || arr.Length == 0)
             {
-                throw new ArgumentException("The array is empty or null.");
+                throw new ArgumentException("The float array is empty or null.");
             }
 
             float maximum = arr[0];
@@ -31,4 +54,5 @@
             return maximum;
         }
     }
+
 }
