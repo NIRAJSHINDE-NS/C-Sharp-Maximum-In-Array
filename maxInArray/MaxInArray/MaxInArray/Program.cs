245e2527c485ca16bca3e432bf11350cd93c5201
NIRAJ <niraj@rajniraj.com>
2023-05-19 19:09:42 +0530

    [Niraj] Added :This is a program to find maximum in array

---
+++ maxInArray/MaxInArray/MaxInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MaxInArray
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            float[] numbers = { 5.2f, 10.7f, 3.1f, 8.9f, 1.4f };
+
+            float maximum = FindMaximum(numbers);
+            Console.WriteLine("The maximum number is: " + maximum);
+        }
+
+        static float FindMaximum(float[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                throw new ArgumentException("The array is empty or null.");
+            }
+
+            float maximum = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > maximum)
+                {
+                    maximum = arr[i];
+                }
+            }
+
+            return maximum;
+        }
+    }
+}
